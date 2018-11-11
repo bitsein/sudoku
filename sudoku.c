54a7da7e28a3ea80b1d452e0fbe4036080a6802c
bitsein <38105635+bitsein@users.noreply.github.com>
2018-11-11 12:56:24 +0900

    Update sudoku.c

--- sudoku.c
+++ sudoku.c
@@ -3,16 +3,14 @@
 #define SUCCESS 1    /* 成功 */
 #define FAILURE 0    /* 失敗 */
 typedef int** sudoku;
-short int list[81]; //空白の番地リスト、十の位に行、一の位に列
+short int list[81]; //空白の番地リスト、行×９＋列
 short int blank_num; //最初の空白の数
 short int num;
 
 sudoku sudoku_new(){ //sudokuのメモリ確保
     sudoku s; int i, j;
     s = malloc(9 * sizeof(int*));
-    for(i=0; i<9; i++){
-        s[i] = malloc(9 * sizeof(int));
-    }
+    for(i=0; i<9; i++) s[i] = malloc(9 * sizeof(int));
     for(i=0; i<9; i++){
         for(j=0; j<9; j++){
             s[i][j] = 0;
@@ -23,9 +21,7 @@ sudoku sudoku_new(){ //sudokuのメモリ確保
 
 void free_sudoku(sudoku s){ //sudokuのメモリ開放
     int i;
-    for(i=0; i<9; i++){
-        free(s[i]);
-    }
+    for(i=0; i<9; i++) free(s[i]);
     free(s);
 }
 
@@ -39,44 +35,30 @@ void print_sudoku(sudoku s){ //sudokuを標準出力に表示
     }
 }
 
-int row_check(sudoku s, int i, int j, int k){ //行方向のチェック
-    int l;
+int rule_check(sudoku s, int i, int j, int k){ //行方向のチェック
+    int l, m, a, b;
     for(l=0; l<9; l++){
-        if(l != j && k == s[i][l]) return FAILURE;
+        if(k == s[i][l]) return FAILURE;
     }
-    return SUCCESS;
-}
-
-int column_check(sudoku s, int i, int j, int k){ //列方向のチェック
-    int l;
     for(l=0; l<9; l++){
-        if(l != i && k == s[l][j]) return FAILURE;
+        if(k == s[l][j]) return FAILURE;
     }
-    return SUCCESS;
-}
-
-int block_check(sudoku s, int i, int j, int k){ //3*3ブロックのチェック
-    int a, b, m, n;
     a = i/3; b = j/3;
-    for(m=3*a; m<3*a+3; m++){
-        for(n=3*b; n<3*b+3; n++){
-            if(i != m && j != n && k == s[m][n]) return FAILURE;
+    for(l=3*a; l<3*a+3; l++){
+        for(m=3*b; m<3*b+3; m++){
+            if(k == s[l][m]) return FAILURE;
         }
     }
     return SUCCESS;
 }
 
 int sudoku_append(sudoku s, int i, int j, int k){ //sudokuの(i, j)にkを埋める
-    if(row_check(s, i, j, k) == FAILURE) return FAILURE;
-    if(column_check(s, i, j, k) == FAILURE) return FAILURE;
-    if(block_check(s, i, j, k) == FAILURE) return FAILURE;
-    
+    if(rule_check(s, i, j, k) == FAILURE) return FAILURE;    
     s[i][j] = k;
     num++;
     if(num == blank_num){
         print_sudoku(s);
-        free_sudoku(s);
-        exit(0);
+        free_sudoku(s); exit(0);
     }
     return SUCCESS;
 }
@@ -101,20 +83,51 @@ void solver(sudoku s){ //再帰的に使う
     }
 }
 
+void check_input(sudoku s, int i, int j, int k){
+    switch(k){
+        case 0:
+            s[i][j] = k;
+            list[blank_num] = i * 9 + j;
+            blank_num++;
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            if(rule_check(s, i, j, k) == FAILURE){
+                printf("RULE_VIOLATION\n");
+                free_sudoku(s); exit(1);
+            }
+            s[i][j] = k;
+            break;
+        default:
+            printf("INPUT_0_to_9\n");
+            free_sudoku(s); exit(1);
+    }
+}
+
 int main(){
     sudoku s;
     s = sudoku_new();
-    int i, j;
+    int i, j, k;
     blank_num = 0; num = 0;
     for(i=0; i<9; i++){
         for(j=0; j<9; j++){
-            scanf("%d", &s[i][j]);
-            if(s[i][j] == 0) {
-                list[blank_num] = i * 9 + j;
-                blank_num++;
-            }
+            scanf("%d", &k);
+            check_input(s, i, j, k);
         }
     }
+    if(blank_num == 0){
+        printf("ALREADY_CLEAR");
+        free_sudoku(s); exit(1);
+    }
     solver(s);
-    printf("CANNOT_SOLVE");
+    printf("CANNOT_SOLVE\n");
+    free_sudoku(s);
+    return 0;
 }
